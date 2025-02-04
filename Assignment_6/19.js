ce79bc6a63bdcba222d3e6973749d0564a03063a
Magan Hara <150593601+Vibhav-Sharma@users.noreply.github.com>
2025-02-04 15:26:09 +0530

    Create 19.js

---
+++ Assignment_6/19.js
@@ -0,0 +1,12 @@
+function findLongestWord(sentence) {
+    const words = sentence.split(' ');
+    let longestWord = '';
+    for (let word of words) {
+        if (word.length > longestWord.length) {
+            longestWord = word;
+        }
+    }
+    
+    return longestWord;
+}
+console.log(findLongestWord("I love programming in JavaScript"));
