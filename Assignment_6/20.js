741d3276e8be1c85ee06a7aeeb2bddb17c46c220
Magan Hara <150593601+Vibhav-Sharma@users.noreply.github.com>
2025-02-04 15:26:24 +0530

    Create 20.js

---
+++ Assignment_6/20.js
@@ -0,0 +1,10 @@
+function countCharacterOccurrences(str) {
+    const charCount = {};
+    for (let char of str) {
+        charCount[char] = (charCount[char] || 0) + 1;
+    }
+    
+    return charCount;
+}
+
+console.log(countCharacterOccurrences("hello")); 
