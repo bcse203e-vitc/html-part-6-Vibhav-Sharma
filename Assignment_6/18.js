ecf2259c4965bb7edb1f172bd4cb5f3352bddedc
Magan Hara <150593601+Vibhav-Sharma@users.noreply.github.com>
2025-02-04 15:25:48 +0530

    Create 18.js

---
+++ Assignment_6/18.js
@@ -0,0 +1,18 @@
+function firstNonRepeatingCharacter(str) {
+    
+    const charCount = {};
+    for (let char of str) {
+        charCount[char] = (charCount[char] || 0) + 1;
+    }
+
+    for (let char of str) {
+        if (charCount[char] === 1) {
+            return char;
+        }
+    }
+
+    return null
+}
+
+
+console.log(firstNonRepeatingCharacter("swiss"));
